3a158ae9f0f61b287e2a3346106cc83dd1571f1f
Raphaël Yancey <raphael@badfile.net>
2018-06-27 18:34:06 +0200

    Fixed version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyky040',
-    version='0.1.2',
+    version='0.1.3',
     description='High-level interface for the KY040 rotary encoder and switch.',
     long_description=long_description,
     long_description_content_type='text/markdown',
