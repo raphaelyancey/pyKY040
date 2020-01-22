39e76bafd23c7cbd5830040531a7e3167375c41e
Raphaël Yancey <raphael@badfile.net>
2020-01-22 14:58:55 +0100

    Re-implemented the evdev code NOTE: switch is handled by another Linux device, not implemented for now

--- pyky040/__main__.py
+++ pyky040/__main__.py
@@ -2,18 +2,22 @@ import sys
 
 args = sys.argv[1:]
 
-dtparam = "dtoverlay=rotary-encoder,pin_a={clk},pin_b={dt},relative_axis=1,steps-per-period=2"
+param_rotary = "dtoverlay=rotary-encoder,pin_a={clk},pin_b={dt},relative_axis=1,steps-per-period=2"
+param_switch = "dtoverlay=gpio-key,gpio={pin},gpio_pull=up,keycode=40"
 
 print("NOTE: this tool will NOT write anything, anywhere, unless you allow it explicitely. Chill!")
 print("NOTE: pin numbering is BCM (not board).\n")
 
-clk = input('Enter the CLK pin of the encoder: ')
+clk = input('Encoder CLK pin: ')
 assert isinstance(clk, int)
-dt = input('Enter the DT pin of the encoder: ')
+dt = input('Encoder DT pin: ')
 assert isinstance(dt, int)
+sw = input('Encoder SWITCH pin: ')
+assert isinstance(sw, int)
 
 print("\n")
-print("To allow pyKY040 to get the encoder events, execute this command:\n")
-print("\tsudo echo \"{}\" >> /boot/config.txt\n".format(dtparam.format(clk=clk, dt=dt)))
+print("To allow pyKY040 to get the encoder events, execute these commands:\n")
+print("\tsudo echo \"{}\" >> /boot/config.txt\n".format(param_rotary.format(clk=clk, dt=dt)))
+print("\tsudo echo \"{}\" >> /boot/config.txt\n".format(param_switch.format(pin=sw)))
 
 print("\nDON'T FORGET TO REBOOT for the changes to take effect!\n")
--- pyky040/pyky040.py
+++ pyky040/pyky040.py
@@ -3,6 +3,7 @@ from time import sleep, time
 import logging
 from threading import Timer
 from os import getenv
+import evdev
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -31,19 +32,30 @@ class Encoder:
     chg_callback = None  # Rotation (either way)
     sw_callback  = None  # Switch pressed
 
-    def __init__(self, CLK=None, DT=None, SW=None, polling_interval=1):
-        if not CLK or not DT:
-            raise BaseException("You must specify at least the CLK & DT pins")
-        self.clk = CLK
-        self.dt = DT
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        if SW is not None:
-            self.sw = SW
-            GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Pulled-up because KY-040 switch is shorted to ground when pressed
-        self.clkLastState = GPIO.input(self.clk)
-        self.polling_interval = polling_interval
+    def __init__(self, CLK=None, DT=None, SW=None, polling_interval=1, device=None):
+
+        if device is not None:
+            try:
+                self.device = evdev.InputDevice(device)
+            except OSError:
+                raise BaseException("The rotary encoder needs to be set up before use: please refer to the README or run `python -m pyky040 setup` for an interactive setup.")
+        else:
+
+            if not CLK or not DT:
+                raise BaseException("You must specify at least the CLK & DT pins")
+
+            self.clk = CLK
+            self.dt = DT
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+            if SW is not None:
+                self.sw = SW
+                GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Pulled-up because KY-040 switch is shorted to ground when pressed
+
+            self.clkLastState = GPIO.input(self.clk)
+            self.polling_interval = polling_interval
 
     def setup(self, **params):
 
@@ -72,68 +84,83 @@ class Encoder:
         if 'sw_debounce_time' in params:
             self.sw_debounce_time = params['sw_debounce_time']
 
+    def _clockwise_tick(self):
+
+        if self.counter + self.step <= self.max_counter:
+            # Loop or not, increment if the max isn't reached
+            self.counter += self.step
+        elif (self.counter + self.step >= self.max_counter) and self.counter_loop is True:
+            # If loop, go back to min once max is reached
+            self.counter = self.min_counter
+
+        if self.inc_callback is not None:
+            self.inc_callback(self.counter)
+        if self.chg_callback is not None:
+            self.chg_callback(self.counter)
+
+    def _anticlockwise_tick(self):
+
+        if self.counter - self.step >= self.min_counter:
+            # Same as for max ^
+            self.counter -= self.step
+        elif (self.counter - self.step <= self.min_counter) and self.counter_loop is True:
+            # If loop, go back to max once min is reached
+            self.counter = self.max_counter
+
+        if self.dec_callback is not None:
+            self.dec_callback(self.counter)
+        if self.chg_callback is not None:
+            self.chg_callback(self.counter)
+
     def watch(self):
 
-        swTriggered = False  # Used to debounce a long switch click (prevent multiple callback calls)
-        latest_switch_call = None
+        if self.device is not None:
 
-        while True:
-            try:
+            for event in self.device.read_loop():
+                if event.type == 2:
+                    if event.value == 1:
+                        self._clockwise_tick()
+                    elif event.value == -1:
+                        self._anticlockwise_tick()
 
-                # Switch part
-                if self.sw_callback:
-                    if GPIO.input(self.sw) == GPIO.LOW:
-                        if not swTriggered:
-                            now = time() * 1000
-                            if latest_switch_call:
-                                if now - latest_switch_call > self.sw_debounce_time:
+        else:
+
+            swTriggered = False  # Used to debounce a long switch click (prevent multiple callback calls)
+            latest_switch_call = None
+
+            while True:
+                try:
+                    # Switch part
+                    if self.sw_callback:
+                        if GPIO.input(self.sw) == GPIO.LOW:
+                            if not swTriggered:
+                                now = time() * 1000
+                                if latest_switch_call:
+                                    if now - latest_switch_call > self.sw_debounce_time:
+                                        self.sw_callback()
+                                else:  # First call
                                     self.sw_callback()
-                            else:  # First call
-                                self.sw_callback()
-                        swTriggered = True
-                        latest_switch_call = now
-                    else:
-                        swTriggered = False
+                            swTriggered = True
+                            latest_switch_call = now
+                        else:
+                            swTriggered = False
 
-                # Encoder part
-                clkState = GPIO.input(self.clk)
-                dtState = GPIO.input(self.dt)
+                    # Encoder part
+                    clkState = GPIO.input(self.clk)
+                    dtState = GPIO.input(self.dt)
 
-                if clkState != self.clkLastState:
+                    if clkState != self.clkLastState:
+                        if dtState != clkState:
+                            self._clockwise_tick()
+                        else:
+                            self._anticlockwise_tick()
 
-                    if dtState != clkState:
+                    self.clkLastState = clkState
+                    sleep(self.polling_interval / 1000)
 
-                        if self.counter + self.step <= self.max_counter:
-                            # Loop or not, increment if the max isn't reached
-                            self.counter += self.step
-                        elif (self.counter + self.step >= self.max_counter) and self.counter_loop is True:
-                            # If loop, go back to min once max is reached
-                            self.counter = self.min_counter
-
-                        if self.inc_callback is not None:
-                            self.inc_callback(self.counter)
-                        if self.chg_callback is not None:
-                            self.chg_callback(self.counter)
-
-                    else:
-
-                        if self.counter - self.step >= self.min_counter:
-                            # Same as for max ^
-                            self.counter -= self.step
-                        elif (self.counter - self.step <= self.min_counter) and self.counter_loop is True:
-                            # If loop, go back to max once min is reached
-                            self.counter = self.max_counter
-
-                        if self.dec_callback is not None:
-                            self.dec_callback(self.counter)
-                        if self.chg_callback is not None:
-                            self.chg_callback(self.counter)
-
-                self.clkLastState = clkState
-                sleep(self.polling_interval / 1000)
-            except BaseException as e:
-                logger.info("Exiting...")
-                logger.info(e)
-                GPIO.cleanup()
-                break
+                except BaseException as e:
+                    logger.info("Exiting...")
+                    logger.info(e)
+                    GPIO.cleanup()
+                    break
         return
